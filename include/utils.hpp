c3bc229843f800b6eb1ca207599608a569f8c737
uncleTes <uncleTes@gmail.com>
2014-12-01 14:31:32 +0100

    Add files "utils.hpp" and "utils.cpp"
    The files "utils.hpp" and "utils.cpp" could be useful to add new
    utility's functions.

---
+++ include/utils.hpp
@@ -0,0 +1,10 @@
+// Function: fileExists
+/**
+	Check if a file exists
+
+@param[in] filename - the namee of the file to check
+
+@return true if the file exists, false otherwise
+**/
+
+bool fileExists(const std::string& filename);
