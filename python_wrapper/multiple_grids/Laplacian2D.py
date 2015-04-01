e46ed78ff76b61d5e85246e0886935b2be47439d
uncleTes <uncleTes@gmail.com>
2015-04-01 13:01:11 +0200

    Modify file "python_wrapper/multiple_grids/Laplacian.py"
    The file "python_wrapper/multiple_grids/Laplacian.py" has the "main"
    function modified.

--- python_wrapper/multiple_grids/Laplacian2D.py
+++ python_wrapper/multiple_grids/Laplacian2D.py
@@ -112,25 +112,79 @@ def set_global_var():
     comm_names = ["comm_zero", "comm_one"]
 # ------------------------------------------------------------------------------
 
+# -------------------------------------MAIN-------------------------------------
 def main():
+
+    group_w = comm_w.Get_group()
+    procs_w = comm_w.Get_size()
+    procs_w_list = range(0, procs_w)
+    zero_list, one_list = split_list_in_two(procs_w_list)
+    group_l = group_w.Incl(zero_list if (rank_w < (procs_w /2 )) else one_list)
+    # Creating differents MPI intracommunicators.
+    comm_l = comm_w.Create(group_l)
+    refinement_levels = 7 if (rank_w < (procs_w / 2)) else 10
+    # Anchor node for PABLO.
+    an = [0, 0, 0] if (rank_w < (procs_w / 2)) else [0.25, 0.25, 0]
+    # Edge's length for PABLO.
+    ed = 1 if (rank_w < (procs_w / 2)) else 0.5
+    # Current intracommunicator's name.
+    comm_name = comm_names[0] if (rank_w < (procs_w / 2)) else comm_names[1]
+    comm_l.Set_name(comm_name)
+
     logger = Logger(__name__).get_logger()
-    logger.info("Started function")
-    x = numpy.array([2, 3, 1, 0])
-    y = numpy.array([1, 4, 1, 0])
+    logger.info("Started function for comm \"" + 
+                str(comm_l.Get_name())         + 
+                "\" and rank \""               +
+                str(comm_l.Get_rank())         +
+                "\".")
 
-    f = ExactSolution2D(x, y)
-    logger.info(f.get_sol())
-    logger.info("Ended function")
-
-def simple_message_log(message):
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler("./Laplacian2D.log")
-    formatter = logging.Formatter("%(message)60s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.info(message)
-
+    pablo = class_para_tree.Py_Class_Para_Tree_D2(an[0]             ,
+                                                  an[1]             ,
+                                                  an[2]             ,
+                                                  ed                ,
+                                                  comm_name + ".log", # Logfile
+                                                  comm_l)             # Comm
+    
+    pablo.set_balance(0, True)
+    
+    for iteration in xrange(1, refinement_levels):
+        pablo.adapt_global_refine()
+    
+    pablo.load_balance()
+    pablo.update_connectivity()
+    pablo.update_ghosts_connectivity()
+    
+    n_octs = pablo.get_num_octants()
+    n_nodes = pablo.get_num_nodes()
+    
+    centers = numpy.empty([n_octs, 2])
+    
+    for i in xrange(0, n_octs):
+        centers[i, :] = pablo.get_center(i)[:2]
+    
+    exact_solution = ExactSolution2D(comm_l, pablo)
+    # Evaluating exact solution in the centers' of the PABLO's cells.
+    exact_solution.evaluate(centers[:, 0], centers[:, 1])
+    
+    vtk = my_class_vtk_02.Py_Class_VTK(exact_solution.sol  , # Data
+                                       pablo               , # Octree
+                                       "./"                , # Dir
+                                       "exact_" + comm_name, # Name
+                                       "ascii"             , # Type
+                                       n_octs              , # Ncells
+                                       n_nodes             , # Nnodes
+                                       4*n_octs)             # Nnodes*pow(2,dim)
+    
+    
+    vtk.print_vtk()
+        
+    logger.info("Ended function for comm \"" + 
+                str(comm_l.Get_name())       + 
+                "\" and rank \""             +
+                str(comm_l.Get_rank())       +
+                "\".")
+# ------------------------------------------------------------------------------
+    
 if __name__ == "__main__":
     simple_message_log("STARTED LOG")
     main()
